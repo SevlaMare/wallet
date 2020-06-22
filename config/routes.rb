abc37a5cad63d01e1a141f2746dfad1423ffe335
mare <sevilamare@gmail.com>
2020-06-22 08:13:21 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :users,        only: [:index, :show, :new, :create]
+  resources :sessions,     only: [:new, :create, :destroy]
+  resources :transactions, only: [:new, :create, :destroy]
+  resources :groups,       only: [:new, :create, :destroy]
   root 'users#show'
 end
