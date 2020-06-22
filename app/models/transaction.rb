328cec7fa3ca993cc111674f7f809cfa95b97cff
mare <sevilamare@gmail.com>
2020-06-22 11:46:04 -0700

    link between user and transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,3 @@
 class Transaction < ApplicationRecord
+  belongs_to :AuthorId, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :transactions
+  # has_many :groups
 end
