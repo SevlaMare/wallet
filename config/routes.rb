1e3463ab5b46fbe43d9c02e66891b3b94e5e0287
mare <sevilamare@gmail.com>
2020-06-22 08:30:15 -0700

    base views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users,        only: [:index, :show, :new, :create]
   resources :sessions,     only: [:new, :create, :destroy]
-  resources :transactions, only: [:new, :create, :destroy]
-  resources :groups,       only: [:new, :create, :destroy]
+  resources :groups,       only: [:index, :new, :create, :destroy]
+  resources :transactions, only: [:index, :new, :create, :destroy]
   root 'users#show'
 end
