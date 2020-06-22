da6241507676153e2bd81cb5d2d1eee1881cee99
mare <sevilamare@gmail.com>
2020-06-22 14:33:06 -0700

    can create group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,24 @@
 class GroupsController < ApplicationController
+  def index
+    # list all groups
+  end
+
+  def show
+    # access all transactions from one group
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
