67282134589d57208173ab93c49c7d95dc7307ea
Mare <sevilamare@gmail.com>
2020-06-29 13:43:15 -0300

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_163940) do
+ActiveRecord::Schema.define(version: 2020_06_29_164127) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_06_22_163940) do
     t.float "amount"
     t.datetime "created_at", null: false
     t.integer "user_id"
+    t.integer "group_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
