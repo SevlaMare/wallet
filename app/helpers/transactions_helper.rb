146dd32852b15599391ab1780be0277a3938c81f
Mare <sevilamare@gmail.com>
2020-06-30 13:27:42 -0300

    transaction index

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,2 +1,5 @@
 module TransactionsHelper
+  def total_amount_with_group
+    Transaction.where.not(group_id: false).sum(:amount)
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,9 @@
 class Transaction < ApplicationRecord
   belongs_to :user
 
-  scope :ordered_by_most_recent, -> { order(created_at: :desc) }
+  scope :sort_most_recent, -> { order(created_at: :desc) }
+
+  def total_amount
+    Transactions.sum(:amount)
+  end
 end
