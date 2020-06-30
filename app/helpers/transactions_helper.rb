f49fad0f455dd4970d315e326dcbe83d1f4b6d7a
Mare <sevilamare@gmail.com>
2020-06-30 14:32:53 -0300

    btn style

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,9 +1,19 @@
 module TransactionsHelper
   def total_amount_with_group
-    Transaction.where.not('group_id' => nil).sum(:amount)
+    Transaction.with_group.sum(:amount)
   end
 
   def total_amount_without_group
-    Transaction.where('group_id' => nil).sum(:amount)
+    Transaction.without_group.sum(:amount)
+  end
+
+  def upcoming_events_list(events)
+    content_tag :ul do
+      events.upcoming.map do |line|
+        concat(
+          content_tag(:li, (link_to line.name, event_path(line.id)))
+        )
+      end
+    end
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,6 @@ class Transaction < ApplicationRecord
   belongs_to :user
 
   scope :sort_most_recent, -> { order(created_at: :desc) }
-  scope :sort_most_recent, -> { order(created_at: :desc) }
+  scope :with_group, -> { where.not('group_id' => nil) }
+  scope :without_group, -> { where('group_id' => nil) }
 end
