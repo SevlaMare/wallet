7a4e2dcc5576ef8d0fd1c2a6d16d45366d8fe439
Mare <sevilamare@gmail.com>
2020-06-30 16:37:22 -0300

    scope into helper

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,7 +1,7 @@
 module GroupsHelper
   def groups_list(groups)
     content_tag :ul do
-      groups.map do |line|
+      groups.sort_alphabetical.map do |line|
         concat(content_tag(:li, (link_to line.name, group_path(line))))
       end
     end
