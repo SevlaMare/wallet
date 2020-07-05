76856a2c0cdf6d1e97efc02a1bba82204d645235
Mare <sevilamare@gmail.com>
2020-07-05 18:18:59 -0300

    allow trasaction without group

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :user
-  belongs_to :group
+  belongs_to :group, optional: true
 
   scope :sort_most_recent, -> { order(created_at: :desc) }
   scope :with_group, -> { where.not('group_id' => nil) }
