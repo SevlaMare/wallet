837ca670851827bf83798be4cf79c5513b08a1a3
Mare <sevilamare@gmail.com>
2020-07-06 17:40:26 -0300

    linter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,10 @@ class ApplicationController < ActionController::Base
   add_flash_types :success
 
   def require_login
-    redirect_to new_session_path,
-      notice: 'You must be logged' unless current_user
+    redirect_to new_session_path, notice: 'You must be logged' unless current_user
   end
 
   def not_logged
-    redirect_to user_path(cookies[:current_user_id]),
-      notice: 'You already logged' if current_user
+    redirect_to user_path(cookies[:current_user_id]), notice: 'You already logged' if current_user
   end
 end
