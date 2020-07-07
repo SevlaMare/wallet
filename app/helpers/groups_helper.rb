592e1dfc6d178ae08092ce91625fc5b9fda37353
Mare <sevilamare@gmail.com>
2020-07-07 20:08:01 -0300

    group#index logic extract

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -8,10 +8,13 @@ end
       groups.map do |line|
           concat(
             content_tag(:div, :class => "list-item") do
-              # content_tag(:a, link_to group_path(line.id))+
+              content_tag(:p, (link_to image_tag(line.icon, class: "icon float"),
+                group_path(line.id), class: 'dark-text') )+
+
+              content_tag(:p, (link_to line.name, group_path(line.id), class: 'dark-text') )+
 
               tag(:hr)+
-              content_tag(:p, line.created_at.strftime("%d %b %Y"), :class => "dark-text")+
+              content_tag(:p, line.created_at.strftime("%d %b %Y"), class: 'dark-text')+
               tag(:br)
             end
           )
