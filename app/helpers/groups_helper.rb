2bd3e60afd15566d25e80e1e8c4f4ef2c0734af4
Mare <sevilamare@gmail.com>
2020-07-07 18:17:22 -0300

    start remove loop from group index to helper

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,11 +1,19 @@
 module GroupsHelper
   def groups_list(groups)
-    content_tag :ul do
-      groups.sort_alphabetical.map do |line|
-        concat(content_tag(:img, line.icon))
-        concat(content_tag(:li, (link_to line.name, group_path(line))))
-        concat(content_tag(:a, line.created_at))
+    content_tag :div, :class => "list" do
+      groups.map do |line| 
+        concat(
+          #content_tag(:a, link_to group_path(line.id))
+          tag(:hr)+
+          content_tag(:p, line.created_at.strftime("%d %b %Y"), :class => "dark-text")
+        )
       end
     end
   end
 end
+
+
+# content_tag(:li, (link_to line.name, event_path(line.id)))
+# concat(content_tag(:img, line.icon))
+# concat(content_tag(:li, (link_to line.name, group_path(line))))
+# concat(content_tag(:a, line.created_at))
