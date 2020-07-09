456fc75c8f34a6fbafde47d1b7e0c843707b5e23
Mare <sevilamare@gmail.com>
2020-07-09 16:30:37 -0300

    extract avatar display to helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,10 @@ module UsersHelper
 
     content_tag(:h1, current_user.name)
   end
+
+  def display_avatar(who)
+    if who.image.attached?
+      image_tag(who.image, class: "avatar center-space")
+    end
+  end
 end
