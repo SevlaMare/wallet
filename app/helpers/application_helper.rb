f19a6389aeffc90be9730d6c5636354e1ffda157
Mare <sevilamare@gmail.com>
2020-07-10 15:25:38 -0300

    extract flash errors to a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
       content_tag(:p, type, class: "alert #{style}") if type
     end.join.html_safe
   end
+
+  def flash_errors
+    return unless flash[:errors]
+
+    content_tag(:ul) do
+      flash[:errors].map do |error|
+        content_tag(:li, error, class: 'alert alert-danger')
+      end.join.html_safe
+    end
+  end
 end
