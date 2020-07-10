874a7af7faf8c42031b32a5d8b1c8d88702caa9f
Mare <sevilamare@gmail.com>
2020-07-10 15:10:16 -0300

    linter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def flash_messages
-    flash.map do |msg|
+    flash.map do |_msg|
       content_tag(:p, notice, class: 'alert alert-danger') if notice
       content_tag(:p, success, class: 'alert alert-success') if success
       content_tag(:p, alert, class: 'alert alert-danger') if alert
