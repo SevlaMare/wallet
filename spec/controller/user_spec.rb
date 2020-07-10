3a6fe3a9504affefdb39130ab8056a56628d7940
Mare <sevilamare@gmail.com>
2020-07-10 14:01:55 -0300

    user controller spec

---
+++ spec/controller/user_spec.rb
@@ -0,0 +1,14 @@
+# rspec ./spec/controller/user_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  let(:user) { User.create(name: 'user01') }
+
+  describe '#show' do
+    it 'block access if not logged in' do
+      get :show, params: { id: user.id }
+      expect(response).to_not have_http_status(:success)
+    end
+  end
+end
