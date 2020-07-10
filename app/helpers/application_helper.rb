0f42dfcc0558edf30de90aa1ac64c20c68ca4158
Mare <sevilamare@gmail.com>
2020-07-10 15:10:00 -0300

    extract flash messages from view to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,9 @@
 module ApplicationHelper
-  # def flash_messagez
-  #   flash.map do |msg|
-  #     content_tag(:p, notice, class: 'alert alert-danger') if notice
-  #     content_tag(:p, success, class: 'alert alert-success') if success
-  #     content_tag(:p, alert, class: 'alert alert-danger') if alert
-  #     end
-  #   end.join.html_safe
-  # end
+  def flash_messages
+    flash.map do |msg|
+      content_tag(:p, notice, class: 'alert alert-danger') if notice
+      content_tag(:p, success, class: 'alert alert-success') if success
+      content_tag(:p, alert, class: 'alert alert-danger') if alert
+    end.join.html_safe
+  end
 end
